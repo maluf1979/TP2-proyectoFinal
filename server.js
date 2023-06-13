4c4ad04348751bbddc312d4fc6b124663f052007
JuanFurgi <juanpifurgi1@gmail.com>
2023-06-12 23:22:21 -0300

    Seed y Login

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import router from "./routes/routes.js";
 import coneccionDb from "./connecctionDb/coneccionDb.js";
 import cors from "cors";
+import roleSeed from "./seed/roleSeed.js";
 
 const app = express();
 const port = process.env.SERVER_PORT;
@@ -12,13 +13,11 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(router);
 
-
-
 await coneccionDb.sync({ force: false }).then(() => {
   app.listen(port, () => {
     console.log("server con express ok http://localhost:8080");
   });
-});
+}).then(roleSeed);
 
 
 
